5a2bbde0c02f73c10b04d5be3c81a0db1ea01bd6
unfl1 <flxmaut10@gmail.com>
2026-08-06 08:49:39 +0900

    햄버거 만들기

---
+++ 프로그래머스/Lv1/햄버거만들기/Solution.java
@@ -0,0 +1,26 @@
+package 프로그래머스.Lv1;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+        int[] ingre = new int[ingredient.length];
+        
+        int point = 0;
+        for (int num : ingredient){
+          ingre[point++]=num;
+
+          if (point>=4){
+            if (ingre[point-4]==1 && 
+                ingre[point-3]==2 && 
+                ingre[point-2]==3 && 
+                ingre[point-1]==1){
+              answer++;
+              point-=4;
+            } 
+          }
+        }
+        return answer;
+    }
+}
+
