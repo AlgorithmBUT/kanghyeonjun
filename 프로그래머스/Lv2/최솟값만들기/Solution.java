9e82c996324908066f044dae2c3f164186801cac
unfl1 <flxmaut10@gmail.com>
2026-08-08 10:42:10 +0900

    최솟값 만들기

---
+++ 프로그래머스/Lv2/최솟값만들기/Solution.java
@@ -0,0 +1,19 @@
+package 프로그래머스.Lv2.최솟값만들기;
+
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for (int i=0; i<A.length; i++){
+            answer+=A[i]*B[B.length-i-1];
+        }
+        return answer;
+    }
+}
