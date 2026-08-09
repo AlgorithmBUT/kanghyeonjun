c493dd11027bcc7b80d07bb5c42c62a75f8cc7e2
unfl1 <flxmaut10@gmail.com>
2026-08-09 10:51:25 +0900

    짝지어 제거하기

---
+++ 프로그래머스/Lv2/숫자의표현/Solution.java
@@ -0,0 +1,24 @@
+package 프로그래머스.Lv2.숫자의표현;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int start=1; start<=n; start++){
+            int tmp=0;
+            for (int i=start; i<=n; i++){
+                tmp+=i;
+
+                if(tmp==n){
+                    answer++;
+                    break;
+                }
+
+                if (tmp>n){
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
