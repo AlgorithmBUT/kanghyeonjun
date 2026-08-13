bb3058b5d878dbcdad4e4ac9d44ca27879db9f28
unfl1 <flxmaut10@gmail.com>
2026-08-13 14:07:58 +0900

    배열에서 최대값 찾기 ver2

--- 백트래킹연습/practice5.java
+++ 백트래킹연습/practice5.java
@@ -2,19 +2,22 @@ package 백트래킹연습;
 
 //크기가 n인 int[] arr에서 최대값 찾기
 
-public class practice4 {
+public class practice5 {
+  static int max, target;
+  static int[] arr;
+  
   public static void main(String[] args) {
     int n = 10;
-    int[] arr = {11,45,32,81,93,6,5,27,72,56};
+    arr = new int[] {11,45,32,81,93,6,5,27,72,56};
 
-    System.out.println(rec(0, arr, 0, 10));
+    System.out.println(rec(0));
   }
 
-  static int rec (int start, int[] arr, int max, int target){
+  static int rec (int start){
     if (start==target){
       return max;
     }
     max=Math.max(max,arr[start]);
-    return rec(start+1, arr, max, target);
+    return rec(start+1);
   }
 }
