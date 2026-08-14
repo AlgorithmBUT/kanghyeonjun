a5d5a1c98ea4bdfd289fa6e40214532bc46ceac4
unfl1 <flxmaut10@gmail.com>
2026-08-14 15:16:56 +0900

    중복 순열

---
+++ 프로그래머스/Lv2/피로도/Solution.java
@@ -0,0 +1,23 @@
+package 프로그래머스.Lv2.피로도;
+
+class Solution {
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        return answer;
+    }
+
+    static void dfs(int[][] arr, int curp, int cnt){
+      if (curp<0){
+        return;
+      }
+
+      if (curp==0){
+        
+      }
+
+      if (curp>=arr[0]){
+        curp+=arr[1];
+      }
+      
+    }
+}
