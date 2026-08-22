e0819d25cf5329f821225fc096fe7bee828c3617
unfl1 <flxmaut10@gmail.com>
2026-08-23 08:40:44 +0900

    정수 삼각형

---
+++ 프로그래머스/Lv3/정수삼각형/Solution.java
@@ -0,0 +1,20 @@
+package 프로그래머스.Lv3.정수삼각형;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+
+        for (int i=1; i<triangle.length; i++){
+            triangle[i][0]+=triangle[i-1][0];
+            triangle[i][triangle[i].length-1]+=triangle[i-1][triangle[i-1].length-1];
+            for (int j=1; j<triangle[i].length-1;j++){
+                triangle[i][j]+=Math.max(triangle[i-1][j-1],triangle[i-1][j]);
+            }
+        }
+
+        answer = Arrays.stream(triangle[triangle.length-1]).max().getAsInt();
+        return answer;
+    }
+}
