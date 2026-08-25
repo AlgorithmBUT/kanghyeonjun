725f86700e8c451e3284a36580a4c0c09570c72f
unfl1 <flxmaut10@gmail.com>
2026-08-25 15:08:32 +0900

    외벽 점검 임시2

--- 프로그래머스/Lv3/외벽점검/Solution.java
+++ 프로그래머스/Lv3/외벽점검/Solution.java
@@ -8,11 +8,19 @@ class Solution {
     static int[] sWeak;
     static int[] sDist;
     static int N;
+    static int ans;
 
     public int solution(int n, int[] weak, int[] dist) {
         int answer = 0;
         sWeak=weak;
+
+        int [] arrDist = Arrays.stream(dist)
+                                .boxed()
+                                .sorted(Comparator.reverseOrder())
+                                .mapToInt(Integer::intValue)
+                                .toArray();
         
+        sDist = arrDist;
         N=n;
         visited = new boolean[n];
         Arrays.fill(visited,true);
@@ -23,7 +31,27 @@ class Solution {
         return answer;
     }
 
-    static void dfs(int[] weak){
+    static void dfs(int cnt){
+        if (cnt>ans){
+            return;
+        }
 
+        for(int j : sWeak){
+            if (!visited[j]){
+                List<Integer> blank = new ArrayList<>();
+                for (int k=j; k<j+cnt; k++){
+                    k%=N;
+                    if (!visited[j]){
+                        blank.add(k);
+                        visited[k]=true;
+                    }
+                }
+                dfs (cnt+1);
+                for (int l : blank){
+                    visited[l]=false;
+                }
+            }
+        
+        }
     }
 }
