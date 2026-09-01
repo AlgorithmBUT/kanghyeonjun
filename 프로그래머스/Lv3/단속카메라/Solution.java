e865cd14e5b6bc8ee548a0e6b00628cbe09d3e96
unfl1 <flxmaut10@gmail.com>
2026-09-01 10:55:14 +0900

    단속카메라

---
+++ 프로그래머스/Lv3/단속카메라/Solution.java
@@ -0,0 +1,26 @@
+package 프로그래머스.Lv3.단속카메라;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+
+        int cur = -30000;      // 카메라 위치
+        int camera = 0;   // 카메라 개수
+
+        Arrays.sort(routes, (a,b) ->{
+          return Integer.compare(a[1], b[1]);
+        });
+
+        for (int i=0; i<routes.length; i++){
+          if (routes[i][0]<=cur){
+            continue;
+          } else {
+            cur=routes[i][1];
+            camera++;
+          }
+        }
+
+        return camera;
+    }
+}
