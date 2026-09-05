d8f460a6b9a26f3bcf78b61b03d830d9a5ca1ef0
unfl1 <flxmaut10@gmail.com>
2026-09-05 16:39:25 +0900

    상호의 배틀필드

--- swea/D3/상호의배틀필드/Solution.java
+++ swea/D3/상호의배틀필드/Solution.java
@@ -1,132 +1,156 @@
-package swea.D3.상호의배틀필드;
-
 import java.util.*;
 import java.io.*;
 
-
 public class Solution {
 
-  static BufferedReader br;
-  static StringTokenizer st;
-  static char[][] board;
+    static BufferedReader br;
+    static StringTokenizer st;
 
-  static int H;
-  static int W;
-  static int dir; // 방향
-  static int y;   // 현재 y좌표
-  static int x;   // 현재 x좌표
+    static char[][] board;
 
-  static int [] dy = {-1,0,1,0};
-  static int [] dx = {0,1,0,-1};
+    static int H;
+    static int W;
 
-  static StringBuilder sb; 
+    static int y;
+    static int x;
+    static int dir;
 
-  public static void main(String[] args) throws Exception {
-    br = new BufferedReader(new InputStreamReader(System.in));
-    int T = Integer.parseInt(br.readLine());
+    // 상 우 하 좌
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
 
-    for (int tc=1; tc<=T; tc++){
-      st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
 
-      H = Integer.parseInt(st.nextToken());
-      W = Integer.parseInt(st.nextToken());
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-      board = new char [H][W];
+        int T = Integer.parseInt(br.readLine());
 
-      int sY=0, sX=0;
+        for (int tc = 1; tc <= T; tc++) {
 
-      // board 초기화
-      for (int i=0; i<H; i++){
-        char[] tmp = br.readLine().toCharArray();
+            st = new StringTokenizer(br.readLine());
 
-        for (int j=0; j<W; j++){
-          board[i][j]=tmp[j];
-          if (board[i][j]=='<' || board[i][j]=='>' || board[i][j]=='^' || board[i][j]=='v'){
-            sY=i;
-            sX=j;
+            H = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
 
-            // 첫 방향 저장
-            if (board[i][j]=='^'){
-              dir=0;
-            } else if (board[i][j]=='>'){
-              dir=1;
-            } else if (board[i][j]=='v'){
-              dir=2;
-            } else if (board[i][j]=='<'){
-              dir=3;
+            board = new char[H][W];
+
+            // 맵 입력 + 탱크 위치, 방향 찾기
+            for (int i = 0; i < H; i++) {
+                board[i] = br.readLine().toCharArray();
+
+                for (int j = 0; j < W; j++) {
+
+                    if (board[i][j] == '^') {
+                        y = i;
+                        x = j;
+                        dir = 0;
+                    } else if (board[i][j] == '>') {
+                        y = i;
+                        x = j;
+                        dir = 1;
+                    } else if (board[i][j] == 'v') {
+                        y = i;
+                        x = j;
+                        dir = 2;
+                    } else if (board[i][j] == '<') {
+                        y = i;
+                        x = j;
+                        dir = 3;
+                    }
+                }
             }
-          }
+
+            int N = Integer.parseInt(br.readLine());
+            char[] order = br.readLine().toCharArray();
+
+            for (int i = 0; i < N; i++) {
+
+                char command = order[i];
+
+                if (command == 'S') {
+                    shoot();
+                    continue;
+                }
+
+                // 방향 변경
+                switch (command) {
+                    case 'U':
+                        dir = 0;
+                        board[y][x] = '^';
+                        break;
+
+                    case 'R':
+                        dir = 1;
+                        board[y][x] = '>';
+                        break;
+
+                    case 'D':
+                        dir = 2;
+                        board[y][x] = 'v';
+                        break;
+
+                    case 'L':
+                        dir = 3;
+                        board[y][x] = '<';
+                        break;
+                }
+
+                // 다음 위치
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
+
+                // 평지라면 이동
+                if (checkArr(ny, nx) && board[ny][nx] == '.') {
+
+                    char tank = board[y][x];
+
+                    board[y][x] = '.';
+
+                    y = ny;
+                    x = nx;
+
+                    board[y][x] = tank;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("#").append(tc).append(" ");
+
+            for (int i = 0; i < H; i++) {
+                sb.append(board[i]).append("\n");
+            }
+
+            System.out.print(sb);
         }
-      }
+    }
 
-      // 명령 처리
-      y=sY;
-      x=sX;
+    static boolean checkArr(int y, int x) {
+        return y >= 0 && y < H && x >= 0 && x < W;
+    }
 
-      int N = Integer.parseInt(br.readLine());
-      char[] order = br.readLine().toCharArray();
-      for (int i=0; i<N; i++){
-        if (order[i]=='S'){
-          shoot (y, x, dir);
-        } else {
-          changeDir(order[i]);
-          move ();
+    static void shoot() {
+
+        int sy = y;
+        int sx = x;
+
+        while (true) {
+
+            sy += dy[dir];
+            sx += dx[dir];
+
+            if (!checkArr(sy, sx)) {
+                break;
+            }
+
+            if (board[sy][sx] == '#') {
+                break;
+            }
+
+            if (board[sy][sx] == '*') {
+                board[sy][sx] = '.';
+                break;
+            }
         }
-      }
-
-      sb = new StringBuilder();
-      sb.append("#" + tc + " ");
-      for (int i=0; i<H; i++){
-        for (int j=0; j<W; j++){
-          sb.append(board[i][j]);
-        }
-        sb.append("\n");
-      }
-
-      System.out.println(sb);
     }
-  }
-
-    static boolean checkArr(int y, int x){
-    if (y<0 || y>=H || x<0 || x>=W){
-      return false;
-    }
-    return true;
-  }
-
-  static int changeDir(char ch){
-    if (ch=='U'){
-      return dir=0;
-    } else if (ch=='R'){
-      return dir=1;
-    } else if (ch=='D'){
-      return dir=2;
-    }
-    return dir=3;
-  }
-
-  static void move (){
-    int ny = y+dy[dir];
-    int nx = x+dx[dir];
-
-    if (checkArr(ny,nx) && board[ny][nx]=='.'){
-      board[y][x]='.';
-      y=ny;
-      x=nx;
-      board[ny][nx]=
-    }
-  }
-
-  static void shoot(int y, int x, int dir){
-    while (true){
-      int ny = y+dy[dir];
-      int nx = x+dx[dir];
-      if (checkArr(ny,nx)){
-        if (board[ny][nx]=='*'){
-          board[ny][nx]='.';
-          break;
-        }
-      }
-    }
-  }
 }
